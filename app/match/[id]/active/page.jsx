3567a500ffa10991b17463f81e86deb41dc4ad36
yahirPC <lughxdr@gmail.com>
2024-01-11 20:21:18 -0600

    match active dosn't exist

--- app/match/[id]/active/page.jsx
+++ app/match/[id]/active/page.jsx
@@ -26,6 +26,7 @@ export async function generateMetadata({ params }) {
 
 export default async function Home({ params }) {
   const match = await getMatch(params.id);
+  
   const images = await championsImage();
   const items_image = await itemsImage();
 
@@ -33,7 +34,7 @@ export default async function Home({ params }) {
   const second = []
 
   match.map(player => {
-    if (player.taskForce ==1 ) first.push(player)
+    if (player.taskForce == 1) first.push(player)
     else second.push(player)
   })
 
@@ -41,51 +42,62 @@ export default async function Home({ params }) {
     <div className="flex flex-col min-h-screen">
       <div className="container mt-4">
         <section className="max-w-4xl w-full mx-auto grid gap-4 grid-cols-1">
-          <div className="">
-            <span className="text-2xl font-bold">
-              {match[0].mapGame}
-            </span>
-            <span className="ml-2 text-gray-500">
-              {match[0].playerRegion}
-            </span>
-          </div>
-          <div className="flex flex-col mb-4">
-            <div className="overflow-x-auto">
-              <div className="inline-block min-w-full py-2 align-middle">
-                <div className="overflow-hidden dark:border-gray-700 md:rounded-2xl">
-                  <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-                    <tbody className="divide-y divide-gray-800">
-                      <UserMatchActiveCard
-                        players={first}
-                        images={images}
-                      />
-                      <tr className={`bg-white dark:divide-gray-700 dark:bg-gray-900`}>
-                        <td className="p-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
-
-                        </td>
-                        <td className="p-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
-
-                        </td>
-                        <td className="p-4 text-2xl font-bold text-gray-500 dark:text-gray-300 whitespace-nowrap text-center">
-                          vs
-                        </td>
-                        <td className="p-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
-
-                        </td>
-                        <td className="p-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
-
-                        </td>
-                      </tr>
-                      <UserMatchActiveCard
-                        players={second}
-                        images={images}
-                      />
-                    </tbody>
-                  </table>
+          {match.length === 1 && <div className="max-w-4xl w-full mx-auto grid gap-4 grid-cols-1">
+            <div className={`bg-gray-900 border border-red-800 shadow-lg rounded-2xl p-4`}>
+              <div className="grid grid-cols-2">
+                <div className="text-base text-gray-100 font-medium">
+                  This match doesn&apos;t exist or has already finished
                 </div>
               </div>
             </div>
-          </div>
+          </div>}
+          { match.length > 1 && <div>
+            <div className="">
+              <span className="text-2xl font-bold">
+                {match[0].mapGame}
+              </span>
+              <span className="ml-2 text-gray-500">
+                {match[0].playerRegion}
+              </span>
+            </div>
+            <div className="flex flex-col mb-4">
+              <div className="overflow-x-auto">
+                <div className="inline-block min-w-full py-2 align-middle">
+                  <div className="overflow-hidden dark:border-gray-700 md:rounded-2xl">
+                    <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                      <tbody className="divide-y divide-gray-800">
+                        <UserMatchActiveCard
+                          players={first}
+                          images={images}
+                        />
+                        <tr className={`bg-white dark:divide-gray-700 dark:bg-gray-900`}>
+                          <td className="p-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
+
+                          </td>
+                          <td className="p-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
+
+                          </td>
+                          <td className="p-4 text-2xl font-bold text-gray-500 dark:text-gray-300 whitespace-nowrap text-center">
+                            vs
+                          </td>
+                          <td className="p-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
+
+                          </td>
+                          <td className="p-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
+
+                          </td>
+                        </tr>
+                        <UserMatchActiveCard
+                          players={second}
+                          images={images}
+                        />
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>}
         </section>
       </div>
     </div>
